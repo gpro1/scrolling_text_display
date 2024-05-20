b04fb31157208b60e0bb3bd5643e73f48a42ddd3
gpro1 <gpro1@rogers.com>
2024-05-20 12:50:00 -0400

    instantiated i2c and display driver

--- display_driver.vhd
+++ display_driver.vhd
@@ -5,6 +5,8 @@ use ieee.numeric_std.all;
 entity display_driver is
  port (
 	i_clk				: in std_logic;
+	i_i2c_data		: in unsigned(7 downto 0); 
+	i_i2c_rdy		: in std_logic;
 	o_i2c_en			: out std_logic; 
 	o_i2c_addr_rw	: out unsigned(7 downto 0);
 	o_i2c_data		: out unsigned(7 downto 0);
