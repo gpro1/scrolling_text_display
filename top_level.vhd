515d1d920848c8371987adcca5f451590b9260e2
gpro1 <gpro1@rogers.com>
2024-05-20 11:12:46 -0400

    added top level

---
+++ top_level.vhd
@@ -0,0 +1,21 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity top_level is
+ port (
+	i_clk_50	: in std_logic;
+	io_sda	: inout std_logic;
+	o_scl		: out std_logic
+	);
+end top_level;
+
+
+architecture rtl of top_level is
+
+
+
+begin
+
+
+end rtl;
